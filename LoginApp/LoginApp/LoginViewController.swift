ee75268bc9504253c3b5f83f6da5e4cde645964d
shaileshsrigiri <51079345+shaileshsrigiri@users.noreply.github.com>
2024-11-06 22:48:10 -0500

    Add files via upload

---
+++ LoginApp/LoginApp/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Shailesh Srigiri on 11/5/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var UsernameTextField: UITextField!
+    
+
+    
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+    @IBAction func LoginAction(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func SignUpAction(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+
+}
